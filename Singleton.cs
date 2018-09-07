a774cbfd799711f0423ceb8b945accfa1d87c487
exuberanteasy <exuberanteasy@gmail.com>
2018-09-07 10:42:22 +0800

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -13,3 +13,35 @@ class SingletonA {
         instance = this;
     }
 }
+
+//============================================================
+
+using System;
+
+class sealed SingletonB {
+    private static SingletonB instance = null;
+
+    public static SingletonB Instance {
+        get {
+            if (instance == null) {
+                instance = new SingletonB();
+            }
+            return instance;
+            // 或寫成下列寫法
+            // return instance ?? (instance = new SingletonB());
+        }
+    }
+
+    private SingletonB() { }
+}
+
+
+
+
+
+//============================================================
+https://blog.davy.tw/archives/413
+
+
+
+
