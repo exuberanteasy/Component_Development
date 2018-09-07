4ea5c46e2a4b15b5be00601c1ba1741c9ea56648
exuberanteasy <exuberanteasy@gmail.com>
2018-09-07 09:22:06 +0800

    Create IDisposable機制.cs

---
+++ IDisposable機制.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace m2_pp_27_IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("開始");
+
+            using (MyPS mp = new MyPS()/* ; */ )
+            {
+                string gidStr = Guid.NewGuid().ToString();
+                mp.WriteLine(gidStr);
+                
+                
+            }// 離開using { }時會自動呼叫mp.Dispose()
+            Console.WriteLine("about");
+        }
+    }
+    class MyPS : IDisposable
+    {
+        public void Dispose()
+        {
+            //釋放物件控制的資源
+            sw.Close();
+        }
+        public void WriteLine(string s)
+        {
+            sw.WriteLine(s);
+        }
+        public MyPS()
+        {
+            sw = new StreamWriter(@"msit999.txt");
+        }
+        StreamWriter sw;
+    }
+    
+}
